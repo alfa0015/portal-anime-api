3c97b34d1cbd474f514cfe32ea9d282f8e3ab565
Rafael Alfaro <alfa0015.rafael@gmail.com>
2018-02-13 21:59:48 -0600

    Configure Devise response json
    skip de ruta de session para devise y adicion de respont json a las acciones de devise ademas de adicionar dookeper a la ruta de api/v1/

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,5 +276,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.navigational_formats = ['*/*', :json]
+  config.navigational_formats = []
+  config.parent_controller = 'ActionController::Base'
 end
