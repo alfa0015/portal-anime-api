be42447d5c1d3896e8a369ab6f29d3c844baf455
Rafael Alfaro <alfa0015.rafael@gmail.com>
2018-02-13 19:31:09 -0600

    Adicion de modulo de authentificacion con outh2
    Se instalo devise para el manejo de usuarios y dookeper para manejar el protocolo outh2 y asi manejar el authentificacion de la API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  use_doorkeeper do
+    skip_controllers :applications, :authorized_applications, :authorizations
+  end
+  devise_for :users
   namespace :api do
     namespace :v1 do
+      # use_doorkeeper do
+      #   skip_controllers :applications, :authorized_applications, :authorizations
+      # end
       get 'posts', to: 'posts#index'
     end
   end
