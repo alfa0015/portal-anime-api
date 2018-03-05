80affb1c4549dc98a7a2fcc6fe6e4bc86bbadf4e
Rafael Alfaro <alfa0015.rafael@gmail.com>
2018-03-05 13:52:24 -0600

    Create resource animes
    Create resource anime and test suit

---
+++ app/controllers/api/v1/animes_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::AnimesController < ApplicationController
+  before_action :doorkeeper_authorize!, except: [:index,:show]
+  before_action :set_anime, only: [:show, :update, :destroy]
+
+  # GET /animes
+  # GET /animes.json
+  def index
+    @animes = Anime.all
+  end
+
+  # GET /animes/1
+  # GET /animes/1.json
+  def show
+  end
+
+  # POST /animes
+  # POST /animes.json
+  def create
+    @anime = Anime.new(anime_params)
+
+    if @anime.save
+      render :show, status: :created
+    else
+      render json: @anime.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /animes/1
+  # PATCH/PUT /animes/1.json
+  def update
+    if @anime.update(anime_params)
+      render :show, status: :ok
+    else
+      render json: @anime.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /animes/1
+  # DELETE /animes/1.json
+  def destroy
+    @anime.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_anime
+      @anime = Anime.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def anime_params
+      params.require(:anime).permit(:name, :synopsis, :sessions, :episodes)
+    end
+end
---
+++ app/models/anime.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: animes
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  synopsis   :text
+#  sessions   :integer
+#  episodes   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Anime < ApplicationRecord
+
+  validates :name, presence: true
+  validates :synopsis, presence: true
+  validates :sessions, presence: true
+  validates :episodes, presence: true
+
+  validates :name, uniqueness: true
+
+  validates :name, length: { in: 10..20 }
+  validates :synopsis, length: { minimum: 10 }
+
+  validates :sessions, numericality: { only_integer: true }
+  validates :episodes, numericality: { only_integer: true }
+end
