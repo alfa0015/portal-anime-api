84cc3252f5a79d477ce17c2a59782a629dabde8b
Rafael Alfaro <alfa0015.rafael@gmail.com>
2019-03-13 22:35:45 -0600

    permit all orogin ws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://portal-anime.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'https://portal-anime.herokuapp.com']
+  config.action_cable.allowed_request_origins = [ 
+    'https://portal-anime.herokuapp.com',
+    /https:\/\/*/
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
